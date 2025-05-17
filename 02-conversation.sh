f893d7b04210fedeac59eaefb7c31d7ed93647a9
MohanMalli <mmohan0140@gmail.com>
2025-05-17 22:33:43 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo " Ravi : Hey hellow Gopi, How about you? "
-echo " Gopi :: Hey Ravi , I'm  doing good .
-echo " Ravi : where are going ?
-echo " Gopi :: yeah i'm going to gym , could you join with me ?
-echo " Ravi : yep !!! you carry on man , I having some work .
+echo " Gopi :: Hey Ravi , I'm  doing good ."
+echo " Ravi : where are going ?"
+echo " Gopi :: yeah i'm going to gym , could you join with me ?"
+echo " Ravi : yep !!! you carry on man , I having some work ."
