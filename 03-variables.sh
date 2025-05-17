2c9d904314cc9bf2595b2c569db3b46ddfe2ec2d
MohanMalli <mmohan0140@gmail.com>
2025-05-17 22:47:59 +0530

    hello world started

--- 03-variables.sh
+++ 03-variables.sh
@@ -7,4 +7,4 @@ echo " $PERSON1 : Hey hellow $PERSON2, How about you? "
 echo " $PERSON2 :: Hey $PERSON1, I'm  doing good ."
 echo " $PERSON1 : where are you going ?"
 echo " $PERSON2 :: yeah i'm going to gym , could you join with me ?"
-echo " $PERSON1 : yep !!! you carry on man , I having some work .
+echo " $PERSON1 : yep !!! you carry on man , I having some work."
