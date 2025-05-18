866fb6120059c7c707a950865d1828c9b989d06f
MohanMalli <mmohan0140@gmail.com>
2025-05-18 05:34:53 +0530

    hello world started

---
+++ 07-data-types.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+
+TIMESTAMP=$(Date) # here date value will store in TIMESTAMP
+
+echo " Script execute at : $TIMESTAMP "
+
+sum = $(($NUMBER1+$NUMBER2)) # here add two numbers
+
+echo " sum of $NUMBER1 and $NUMBER is : $SUM "
+
