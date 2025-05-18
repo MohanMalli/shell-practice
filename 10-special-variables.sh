d865b997d37d678b43dfcf2d46af8c3ccc715aa2
MohanMalli <mmohan0140@gmail.com>
2025-05-18 06:49:55 +0530

    hello world started

--- 10-special-variables.sh
+++ 10-special-variables.sh
@@ -6,6 +6,6 @@ echo "Script name: $0"
 echo "current Directoru: $PWD"
 echo "home directory of user: $HOME"
 echo "user running the Script: $USER"
-echo "PID of the current script : $$"
+echo "PID of the current script: $$"
 sleep 10 &
 echo "PID of last command in background: $!"
