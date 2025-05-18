aa2da951ba48b510ee07bd5df2cc5295053de298
MohanMalli <mmohan0140@gmail.com>
2025-05-18 06:47:43 +0530

    hello world started

--- 10-special-variables.sh
+++ 10-special-variables.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "All variables passed to script: $@"
+echo "number of variables passed: $#"
+echo "Script name: $0"
+echo "current Directoru: $PWD"
+echo "home directory of user: $HOME"
+echo "user running the Script: $USER"
+echo "PID of the current script : $$"
+sleep 10 &
+echo "PID of last command in background: $!"
