f36517c47a46d5009b6eb812c66fbbaa5ae9711b
MohanMalli <mmohan0140@gmail.com>
2025-05-18 10:01:54 +0530

    hello world started

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]
 then 
-     echo " Given number $NUMBER is less then 10 "
+     echo "Given number $NUMBER is less then 10"
 else
-     echo " Given number $NUMBER is not less then 10 "
+     echo "Given number $NUMBER is not less then 10"
 if    
