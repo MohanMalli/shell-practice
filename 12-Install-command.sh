d747c1722d5112de887adddce2d88e3aa9db3612
MohanMalli <mmohan0140@gmail.com>
2025-05-18 10:56:05 +0530

    hello world stared

--- 12-Install-command.sh
+++ 12-Install-command.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-USERID=$(id -u) # userid = 1001
+#USERID=$(id -u) # userid = 1001
 
-if [ $USERID -ne 0 ] # here 1001 is not equal to 0  -->  ## [1001 =/ 0] ##
-then
+#if [ $USERID -ne 0 ] # here 1001 is not equal to 0  -->  ## [1001 =/ 0] ##
+#then
     echo "ERROR: Please run this Script with root access"
     exit 1 # give other than 0 up to  127
-else 
-    echo "you are running with  root access"
-fi
+#else 
+    #echo "you are running with  root access"
+#fi
 dnf install mysql -y
 
 if [ $? -eq 0 ]  # [ 0 = 0 ]
