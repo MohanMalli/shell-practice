4ab5fc2e85bbad4af33eb3830f3d5fdc26fed63c
MohanMalli <mmohan0140@gmail.com>
2025-05-18 12:21:09 +0530

    hello world started

--- 13-install-command.sh
+++ 13-install-command.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 if [ $? -ne 0 ] #[1 =/ 0 ]
 then 
-  echo "MySQL is not installed ... goining to install it"
+  echo "nginx is not installed ... goining to install it"
 
-  dnf install mysql -y
-fi
+  dnf install nginx -y
 if [ $? -eq 0 ]  # [ 0 = 0 ]
 then
-    echo "Installing MySQL is ... SUCCESS"
+    echo "Installing nginx is ... SUCCESS"
 else
-   echo "Installing MySQL is ... FAILURE"
+   echo "Installing nginx is ... FAILURE"
    exit 1 # give other than 0 up to  127
 fi
+else 
+    echo "nginx is already installed ... nothing to do"
+if     
