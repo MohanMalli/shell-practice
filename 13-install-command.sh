da59535268f5e6a0fbb3dbeea53d8144553635cc
MohanMalli <mmohan0140@gmail.com>
2025-05-18 11:27:39 +0530

    hello world started

---
+++ 13-install-command.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ $? -ne 0 ] #[1 =/ 0 ]
+then 
+echo "MySQL is not installed ... goining to install it"
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]  # [ 0 = 0 ]
+then
+    echo "Installing MySQL is ... SUCCESS"
+else
+   echo "Installing MySQL is ... FAILURE"
+   exit 1 # give other than 0 up to  127
+fi
+else 
+    echo "mysql is already installed"
+
+fi
