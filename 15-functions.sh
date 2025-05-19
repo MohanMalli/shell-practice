c68f01b41b1931e49159265383db4168ecc8f382
MohanMalli <mmohan0140@gmail.com>
2025-05-19 14:55:04 +0530

    hello world started

--- 15-functions.sh
+++ 15-functions.sh
@@ -7,17 +7,18 @@ then
     echo  "your running with root user"
     
 else
-    echo " ERROR : we are running with not root user.Please run with root user" 
+    echo "ERROR : we are running with not root user.Please run with root user" 
     exit 1
-fi       
+fi 
+
 dnf list installed MongoDB
 if [ $? -eq 0]
 then 
     echo " MongoDB already installed"
-
+    exit 1
 else
    echo "MongoDB is not installed ... now going to install"
    dnf install MongoDB -y
-   exit 1
+   
 fi 
 
