3556c09d00fa97dc275080158b88f2acaa1b0ea4
MohanMalli <mmohan0140@gmail.com>
2025-05-19 11:16:39 +0530

    hello world started

--- 13-install-command.sh
+++ 13-install-command.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+USERID=$(id -u) # userid = 1001
+
+if [ $USERID -ne 0 ] # here 1001 is not equal to 0  -->  ## [1001 =/ 0] ##
+then
+    echo "ERROR: Please run this Script with root access"
+    exit 1 # give other than 0 up to  127
+else 
+    echo "you are running with  root access"
+fi
+dnf install mysql -y
+
 if [ $? -ne 0 ] #[1 =/ 0 ]
 then 
-  echo "nginx is not installed ... goining to install it"
+  echo "mysql is not installed ... goining to install it"
 
-  dnf install nginx -y
+  dnf install mysql -y
 if [ $? -eq 0 ]  # [ 0 = 0 ]
 then
-    echo "Installing nginx is ... SUCCESS"
+    echo "Installing mysql is ... SUCCESS"
 else
-   echo "Installing nginx is ... FAILURE"
+   echo "Installing mysql is ... FAILURE"
    exit 1 # give other than 0 up to  127
 fi
 else 
-    echo "nginx is already installed ... nothing to do"
+    echo "mysql is already installed ... nothing to do"
 fi   
