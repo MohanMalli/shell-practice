1febd233c7724792c025a478ec2061177c685d9b
MohanMalli <mmohan0140@gmail.com>
2025-05-19 10:46:32 +0530

    hello world started

---
+++ 14-install-command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ $? -eq 0 ]  # [ 0 = 0 ]
+then
+    echo "Installing MySQL is ... SUCCESS"
+else
+   echo "Installing MySQL is ... FAILURE"
+   exit 1 # give other than 0 up to  127
+fi
