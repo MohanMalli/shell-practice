625216441570e515c61449fc26d0982e0bfabc05
MohanMalli <mmohan0140@gmail.com>
2025-05-19 14:41:41 +0530

    hello world started

--- 15-functions.sh
+++ 15-functions.sh
@@ -4,9 +4,9 @@ USERID=$( id -u)
 
 if [ $USERID -eq 0 ]
 then
-    echo  your running with root user
-    exit 1
+    echo  :your running with root user"
+    
 else
     echo " ERROR : we are running with not root user.Please run with root user" 
-
+    exit 1
 fi       
