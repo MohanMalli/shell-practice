97761ebef26acf767ea7d4d85aeb571f61b21ea9
MohanMalli <mmohan0140@gmail.com>
2025-06-03 19:29:19 +0530

    hello world started

--- 21-delete-old-logs.sh
+++ 21-delete-old-logs.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SOURCE_DIR=/home/ec2-user/app-logs
+SOURCE_DIR=/home/ec2-user/app.logs
 
 mkdir -p $LOGS_FOLDER
 
@@ -36,11 +36,11 @@ echo "Script started executing at $(date)" | tee -a $LOG_FILE
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
   
- while IFS = read -r filepath
- do
+ while IFS= read -r filepath
+  do
     echo "Deleting file: $filepath" | tee -a $LOG_FILE
-    rm -rf $filepath
- done <<< $FILES_TO_DELETE
+     rm -rf $filepath
+  done <<< $FILES_TO_DELETE
 
  echo "Script executed successfully"
 
