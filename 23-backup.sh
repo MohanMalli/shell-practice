da17066f915e90fcd3577b39440e2c80bf6ba7e0
MohanMalli <mmohan0140@gmail.com>
2025-06-04 20:53:26 +0530

    hello world started

--- 23-backup.sh
+++ 23-backup.sh
@@ -1,57 +1,59 @@
-#!/bin/bash
+ #!/bin/bash
 
 USERID=$(id -u)
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14}
+DAYS=${3:-14} # if DAYS are provided that will be considered, otherwise default 14 days
 
-LOGS_FOLDER="/var/log/sheelscript-logs"
-SCRIPT_NAME="(echo $0 | cut -d "." -f1)"
-LOG_FILE="$LOGS_FOLDER/backup-log"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/backup.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
-if [ $USERID -ne 0 ]
-then
-    echo -e " $R ERROR:: Please run the Script with Root Access $N "
-    exit 1
-else
-    echo -e " $G Your Running with Root Access $N "  
-fi
-
-VALIDATE(){
-if [ $1 -eq 0 ]
-then
-    echo -e  " $2 is ...  $G SUCCESS $N "
-else
-    echo -e  " $2 is ... $R FAILURE $N "
-    exit 1
-fi     
+check_root(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR:: Please run this script with root access $N"
+        exit 1 #give other than 0 upto 127
+    else
+        echo " $G You are running with root access $N" 
+    fi
 }
 
-mkdir -p $LOGS_FOLDER
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
  
+mkdir -p $LOGS_FOLDER
+
 USAGE(){
-    echo -e "$R USAGE:: $N sh 23-backup.sh <source-dir> <destination-dir> <days(optional)>" 
+    echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
     exit 1
 }
 
-if [ $# -lt 2 ]
+if [ $# -lt 2 ] #  Check if at Least One Argument Is Supplied
 then
     USAGE
 fi
 
-if [ ! -d $SOURCE_DIR ]
+if [ ! -d $SOURCE_DIR ] # check the directory is exit or not
 then
     echo -e "$R Source Directory $SOURCE_DIR does not exist. Please check $N"
     exit 1
 fi
 
-if [ ! -d $DEST_DIR ]
+if [ ! -d $DEST_DIR ]  # check the directory is exit or not
 then
     echo -e "$R Destination Directory $DEST_DIR does not exist. Please check $N"
     exit 1
@@ -59,7 +61,7 @@ fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-if [ ! -z "$FILES" ]
+if [ ! -z "$FILES" ]  # check the file is exit or not
 then
     echo "Files to zip are: $FILES"
     TIMESTAMP=$(date +%F-%H-%M-%S)
